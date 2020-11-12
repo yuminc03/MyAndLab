22ed50e1395303298766b2658348338c1f038a70
Yumin Chu <s2019w20@e-mirim.hs.kr>
2020-11-12 20:52:40 +0900

    #7 날짜와 시간을 설정하여 예약하고 예약한 시간이 나오는 기능

--- app/src/main/java/kr/hs/emirim/s2019w20/myandlab/ch06advwidget/DateTimeBookActivity.kt
+++ app/src/main/java/kr/hs/emirim/s2019w20/myandlab/ch06advwidget/DateTimeBookActivity.kt
@@ -1,8 +1,11 @@
 package kr.hs.emirim.s2019w20.myandlab.ch06advwidget
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.*
 import kr.hs.emirim.s2019w20.myandlab.R
@@ -11,17 +14,19 @@ import java.util.*
 
 class DateTimeBookActivity : AppCompatActivity() {
 
+     val TAG = "날짜예약액티비티"
+
     lateinit var chrono : Chronometer
     lateinit var btnStart : Button
     lateinit var btnEnd : Button
     lateinit var rdoCal : RadioButton
     lateinit var rdoTime : RadioButton
     lateinit var calView : CalendarView
-    lateinit var timePicker1: TimePicker
+    lateinit var tPicker: TimePicker
     lateinit var tvDatetime : TextView
 
     var selectYear : Int = 0
-    var selelctMonth : Int = 0
+    var selectMonth : Int = 0
     var selectDay : Int = 0
 
 
@@ -36,12 +41,50 @@ class DateTimeBookActivity : AppCompatActivity() {
         chrono = findViewById(R.id.choronometer1)
         rdoCal = findViewById(R.id.rdoCal)
         rdoTime = findViewById(R.id.rdoTime)
-        timePicker1 = findViewById(R.id.timePicker1)
+        tPicker = findViewById(R.id.timePicker1)
         calView = findViewById(R.id.calendarView1)
         tvDatetime = findViewById(R.id.tvDatetime)
 
-        timePicker1.visibility = View.GONE
         calView.visibility = View.GONE
+        tPicker.visibility = View.GONE
+
+        rdoTime.setOnClickListener{
+            calView.visibility = View.GONE
+            tPicker.visibility = View.VISIBLE
+        }
+
+        rdoCal.setOnClickListener{
+            calView.visibility = View.VISIBLE
+            tPicker.visibility = View.GONE
+            Log.d(TAG, "rdoCal 클릭")
+        }
+
+        btnStart.setOnClickListener{
+            chrono.base = SystemClock.elapsedRealtime()
+            chrono.start()
+            chrono.setTextColor(Color.RED)
+            Log.d(TAG, "btStart 클릭됨")
+        }
+
+        btnEnd.setOnClickListener {
+            chrono.stop()
+            chrono.setTextColor(Color.BLUE)
+            Log.d(TAG, "btnEnd 클릭됨")
+
+            if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M){
+                tvDatetime.text = "" + selectYear + "년" + selectMonth + "월" + selectDay + "일"+ tPicker.hour + "시" + tPicker.minute + "분 예약됨"
+            }
+            else {
+                tvDatetime.text = "" + selectYear + "년" + selectMonth + "월" + selectDay + "일" + tPicker.currentHour + "시" + tPicker.currentMinute + "분 예약됨"
+            }
+        }
+
+        calView.setOnDateChangeListener{view, year, month, dayofMonth ->
+            selectYear = year
+            selectMonth = month + 1
+            selectDay = dayofMonth
+            Log.d(TAG, "오늘은: " + selectYear + "/" + selectMonth + "/" + selectDay)
+        }
 
     }
 }
